a7f2e16c1b4d2cb6048dc085a1dbddcb31dc18aa
Paulo Carabuena <45958147+pawekz@users.noreply.github.com>
2023-11-17 05:21:19 +0800

    added Testing.java

---
+++ src/com/teamspirit/Testing.java
@@ -0,0 +1,10 @@
+package com.teamspirit;
+
+public class Testing {
+    public static void main(String[] args) {
+        System.out.println("Testing...");
+        System.out.println("Testing...");
+        System.out.println("Testing...");
+        System.out.println("Testing...");
+    }
+}
