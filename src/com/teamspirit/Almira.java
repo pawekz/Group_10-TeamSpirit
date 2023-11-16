0ee84601fd6e9e2dba2001f9c5cfad8ce1a41eda
Paulo Carabuena <45958147+pawekz@users.noreply.github.com>
2023-11-17 05:40:21 +0800

    added Almira.java

---
+++ src/com/teamspirit/Almira.java
@@ -0,0 +1,8 @@
+package com.teamspirit;
+
+public class Almira {
+    public static void main(String[] args) {
+        System.out.println("retesting...");
+        System.out.println("it seems that in Intellij, no need to commit on local git, just push to remote");
+    }
+}
