c8f9535d8eddd10438e57454ac5894303be96e19
Paulo Carabuena <45958147+pawekz@users.noreply.github.com>
2023-11-21 05:51:39 +0800

    testing

--- src/com/teamspirit/Main.java
+++ src/com/teamspirit/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("Testing...");
         System.out.println("Testing...");
         System.out.println("Testing...");
+
+        Testing t = new Testing();
     }
 }
--- src/com/teamspirit/Testing.java
+++ src/com/teamspirit/Testing.java
@@ -1,8 +1,8 @@
 package com.teamspirit;
 
 public class Testing {
-    public static void main(String[] args) {
-        System.out.println("Testing...");
+    Testing(){
+        System.out.println("Calling T Testing...");
         System.out.println("Testing...");
         System.out.println("Testing...");
         System.out.println("Testing...");
